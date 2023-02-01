6d5cb0e00e1466752ec359af6e155df0f3677935
anna-szulzik <annaszulzik10@gmail.com>
2023-01-31 21:40:25 -0300

    adiciona @GetMapping ao getAll do Controller

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class PostagemController {
     @Autowired
     private PostagemRespository postagemRespository;
 
+    @GetMapping
     public ResponseEntity<List<Postagem>> getAll() {
         return ResponseEntity.ok(postagemRespository.findAll());
     }
